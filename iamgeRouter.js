677d68e88532be8399f919f576306b7099a4832a
vinoth_872 <vinothkumarp.uplogic@gmail.com>
2024-12-25 11:50:29 +0530

    Image Route

--- iamgeRouter.js
+++ iamgeRouter.js
@@ -1,11 +1,46 @@
 const router = require('express').Router()
-
+const fs = require('fs')
 
 router.get('/',(req,res)=>{
-    res.send({
-        success:true,
-        message:"Image fethed Successfully !"
+
+   try {
+    fs.readFile('./imageFile/nature.jpeg' ,(err,image)=>{
+        if(err){
+            throw new Error(err)
+        }
+        const base64Image = image.toString('base64');
+
+        // Send as JSON with base64-encoded image
+        res.json(
+            // {
+        //   success: true,
+        //   message: "Image fetched successfully!",    
+        //   image: 
+          `data:image/jpeg;base64,${base64Image}`,
+        // }
+    );
     })
+
+    
+   } catch (error) {
+    res.send({
+        success: false,
+        message: error.message || "Internal Server Error !",
+      });
+   }
 })
+
+router.get('/crs',(req,res)=>{
+
+    try {
+     const image  = fs.createReadStream('./imageFile/nature.jpeg')
+     image.pipe(res)
+    } catch (error) {
+     res.send({
+         success: false,
+         message: error.message || "Internal Server Error !",
+       });
+    }
+ })
 
 module.exports = router
